44a30d70bb3dc31f96359d070b0a72a3fd3dc503
Harikrishnan Nandanan <225583@ust-global.com>
2024-07-10 12:36:50 +0530

    nvl library first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NVL } from '@neo4j-nvl/base';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'neo4j-bloom';
+
+  @ViewChild('container') container!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.renderGraph();
+  }
+
+  renderGraph(): void {
+    const nodes = [{ id: '1' }, { id: '2' }];
+    const relationships = [{ id: '12', from: '1', to: '2' }];
+
+    const nvl = new NVL(this.container.nativeElement, nodes, relationships);
+  }
 }
